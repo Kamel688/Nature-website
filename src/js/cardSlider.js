559e1d9462e994e72e331adb7425e44659355adf
Patryk <kamel952122@gmail.com>
2023-03-16 14:26:28 +0100

    Created card slider for location cards in javascript

---
+++ src/js/cardSlider.js
@@ -0,0 +1,64 @@
+const cardBox = document.querySelector('.location__boxes');
+const cards = document.querySelectorAll('.location__box');
+const cardStyles = cards[0].currentStyle || window.getComputedStyle(cards[0]);
+const leftArrow = document.querySelector('.location__left-arrow');
+const rightArrow = document.querySelector('.location__right-arrow');
+
+const sliderSpeed = 3000;
+let cardShift = 0;
+
+const handleCardSlider = () => {
+    cardShift++;
+    changeCard();
+}
+//let startCardSlider = setInterval(handleCardSlider, sliderSpeed);
+
+const changeCard = () => {
+    const cardWidth = cards[0].clientWidth;
+    const cardMargin = parseInt(cardStyles.marginLeft) + parseInt(cardStyles.marginRight); // suma lewego i prawego marginesu
+    const pageWidth = window.innerWidth;
+    cardBox.style.transition = 'transform 0.3s';
+    if(pageWidth < 768){
+        if(cardShift < 0){
+            cardShift = cards.length - 1;
+        }else if(cardShift > cards.length - 1){
+            cardShift = 0;
+        }
+    }else if(pageWidth < 992){
+        if(cardShift < 0){
+            cardShift = cards.length - 2;
+        }else if(cardShift > cards.length - 2){
+            cardShift = 0;
+        }
+    }else if(pageWidth >= 992){
+        if(cardShift < 0){
+            cardShift = cards.length - 3;
+        }else if(cardShift > cards.length - 3){
+            cardShift = 0;
+        }
+    }
+    cardBox.style.transform = `translateX(${-(cardWidth + cardMargin) * cardShift}px)`
+    console.log(cardShift);
+}
+
+const resetCardInterval = () => {
+    clearInterval(startCardSlider);
+    changeCard();
+    startCardSlider = setInterval(handleCardSlider, sliderSpeed);
+}
+
+const handleleftArrow = () => {
+    cardShift--;
+    resetCardInterval();
+}
+const handleRightArrow = () => {
+    cardShift++;
+    resetCardInterval();
+}
+
+
+
+window.addEventListener('resize', resetCardInterval);
+
+leftArrow.addEventListener('click', handleleftArrow);
+rightArrow.addEventListener('click', handleRightArrow);
--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,6 @@ const bars = document.querySelector('.navbar__bars');
 const navbarMobileList = document.querySelector('.navbar__mobile-list');
 const navbarMobileLinks = document.querySelectorAll('.navbar__mobile-list-item');
 
-
 let shift = 0;
 const sliderSpeed = 3000;
 
@@ -21,7 +20,7 @@ navbarMobileLinks.forEach(navbarMobileLink => {
         navbarMobileList.classList.remove('navbar__mobile-list-active');
     })
 })
-burgerBtn.addEventListener('click', handleNav);
+
 
 
 
